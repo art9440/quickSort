f6679caf5b87100773ed7e160deeab112b677cb8
art9440 <art9440@gmail.com>
2023-11-03 12:12:37 +0700

    master

--- quickSort.c
+++ quickSort.c
@@ -1,3 +1,56 @@
+#include <stdio.h>
+#include <malloc.h>
+
+
+void swap(int *a, int *b){
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void quickSort(int* arr, int left, int right)
+{
+    if (left < right) {
+        int mid = arr[(left + right) / 2];
+        int i = left, j = right;
+
+        while (i <= j)
+        {
+
+            while (arr[i] < mid)
+                i++;
+
+            while (arr[j] > mid)
+                j--;
+
+            if (i <= j) {
+                swap(&arr[i], &arr[j]);
+                i++;
+                j--;
+            }
+        }
+        quickSort(arr, left, j);
+        quickSort(arr, i, right);
+    }
+}
+
+
 int main(){
+    int N;
+    scanf("%d", &N);
+    int left = 0, right = N - 1;
+    int *arr = (int*)malloc(N * sizeof(int));
+
+    for (int i = 0; i < N; i++)
+        scanf("%d", &arr[i]);
+
+    quickSort(arr, left, right);
+
+    for(int i = 0; i < N; i++)
+        printf("%d ", arr[i]);
+
+    free(arr);
+
     return 0;
 }
